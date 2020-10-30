a4f707137d85e5c2263d3b3715f428e47c3922f3
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2020-10-29 21:59:14 -0400

    I Started the navbar
    in the working stages, please hold, tech difficulties

---
+++ Home.php
@@ -0,0 +1,27 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+	<link href = "..css\Cool.css" rel="stylesheet" type="text/css">
+	<meta charset="utf-8">
+	<title>Alex's Projects</title>	
+	</head>
+	<body>
+
+		<header>
+		
+			<div class="container">
+			<img src="Pictures/CountryWearThree.png" alt="logo" class="logo">
+				
+					<nav>
+						
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Portfolio</a></li>
+								<li><a href="#">About Me</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+					</nav>
+			</div>
+		</header>
+	</body>
+</html>

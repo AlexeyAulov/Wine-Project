92951723143e54c2a2d3123f5118b5e80fd3c8d1
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2020-10-30 18:16:02 -0400

    Revert "Changed the Navbar Items as well as color"
    This reverts commit d7bef266f9ebf6f38cae87b28a799603e6d14ace.

--- Home.php
+++ Home.php
@@ -1,7 +1,7 @@
 <!doctype html>
 	<html lang="en">
 	<head>
-	<link href = "CSS\Cool.css" rel="stylesheet" type="text/css">
+	<link href = "..css\Cool.css" rel="stylesheet" type="text/css">
 	<meta charset="utf-8">
 	<title>Alex's Projects</title>	
 	</head>
@@ -10,15 +10,15 @@
 		<header>
 		
 			<div class="container">
-			<img src="Pic/CountryWearThree.png" alt="logo" class="logo">
+			<img src="Pictures/CountryWearThree.png" alt="logo" class="logo">
 				
 					<nav>
 						
 							<ul>
-								<li><a href="#"></a>Home</li>
-								<li><a href="#"></a>Catalog</li>
-								<li><a href="#"></a>Log In</li>
-								<li><a href="#"></a>About</a></li>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Portfolio</a></li>
+								<li><a href="#">About Me</a></li>
+								<li><a href="#">Contact</a></li>
 							</ul>
 					</nav>
 			</div>

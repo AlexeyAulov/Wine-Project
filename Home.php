20899b6265932888187aba424595558b77ad07f4
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2020-10-30 21:38:22 -0400

    Navbar Final
    Created Navbar,logo, and Search bar

--- Home.php
+++ Home.php
@@ -1,7 +1,7 @@
 <!doctype html>
 	<html lang="en">
 	<head>
-	<link href = "..css\Cool.css" rel="stylesheet" type="text/css">
+	<link href = "CSS\Cool.css" rel="stylesheet" type="text/css">
 	<meta charset="utf-8">
 	<title>Alex's Projects</title>	
 	</head>
@@ -9,19 +9,22 @@
 
 		<header>
 		
-			<div class="container">
-			<img src="Pictures/CountryWearThree.png" alt="logo" class="logo">
-				
-					<nav>
-						
-							<ul>
-								<li><a href="#">Home</a></li>
-								<li><a href="#">Portfolio</a></li>
-								<li><a href="#">About Me</a></li>
-								<li><a href="#">Contact</a></li>
-							</ul>
-					</nav>
-			</div>
-		</header>
+        <div class="logo">
+            <img src="..\Pic\WinePic.png"></img>
+        </div>
+	
+	</a>
+		<nav>
+			<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Catalog</a></li>
+			<li><a href="#">Cart</a></li>
+			<li><a href="#">Log In/Sign Up</a></li>
+			<li><a href="#">About Us</a></li>
+			<input type ="text" placeholder="Wine Search..."></input>	
+		</ul>
+		</nav>
+		</div>
+  </header>
 	</body>
 </html>

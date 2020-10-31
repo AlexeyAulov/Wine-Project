57fc89c5409eb04feabe9d5ebb57e6fab3bacade
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2020-10-30 22:31:10 -0400

    NavBar Final

---
+++ All_Pages/CG.php
@@ -0,0 +1,30 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+	<link href = "CSS\Cool.css" rel="stylesheet" type="text/css">
+	<meta charset="utf-8">
+	<title>The online Winary</title>	
+	</head>
+	<body>
+
+		<header>
+		
+        <div class="logo">
+            <img src="..\Pic\WinePic.png"></img>
+        </div>
+	
+	</a>
+		<nav>
+			<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Catalog</a></li>
+			<li><a href="#">Cart</a></li>
+			<li><a href="#">Log In/Sign Up</a></li>
+			<li><a href="#">About Us</a></li>
+			<input type ="text" placeholder="Wine Search..."></input>	
+		</ul>
+		</nav>
+		</div>
+  </header>
+	</body>
+</html>

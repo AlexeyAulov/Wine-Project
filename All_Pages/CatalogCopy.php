7e0a856ae84f028de7a5644db6b2f0396b43d962
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2020-11-25 19:03:08 -0500

    Prob with array

--- All_Pages/CatalogCopy.php
+++ All_Pages/CatalogCopy.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 include("Controller\Catalog.inc.php");
 ?>
 <!doctype html>
@@ -59,41 +59,102 @@ include("Controller\Catalog.inc.php");
 
 
 
-  <?php
-
-//$wines=array($query1);
-for ($i=0; $i<$records; $i++){
-   ?>
-    <div action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST" class="main-form needs-validation card-deck">
+  for
+   
+    <div class="card-deck">
       <div class="card bg-secondary text-white">
        <img src="../Pic/Wine_CG_1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title" name='wineName' value=<?php echo $wine_name[$i]; }?>></h5>
+          <h5 class="card-title" name='wineName' value=<?php echo $wine_name??""; ?>></h5>
           <p class="card-text"></p>
           <a href="#" class="btn btn-outline-info">Go somewhere</a>
         </div>
       </div>   
     </div>
+<?php
 
-  <?php
+
+
+  foreach($result as $wines)
+  echo $wine_name;
+
+  
  
  
 if($current_page>1){
 
  ?>
-    <button type="submit" name="Previous" class="btn btn-primary" 
-    value="<?php echo '<a href="CG.php?page='.($current_page-1).'">Previous</a>';?> "></button>
+    <button type="" name="Previous" class="btn btn-primary" 
+    value="<?php echo '<a href="CG.php?page='.($current_page-1).'"></a>'?>" >Previous</button>
    <?php
 }
 if ($current_page<$total_pages){
     ?>
-    <button type="submit" name="Next" class="btn btn-primary" 
-    value="<?php echo'<a href="CG.php?page='.($current_page+1).'">Next</a>';?> "></button>
+    <button type="" name="Next" class="btn btn-primary" 
+    value="<?php echo'<a href="CG.php?page='.($current_page+1).'"></a>'?>">Next</button>
   
   <?php  
 }
 ?>
 
+<!--
+
+ if (isset($_POST["sendPhoto"])){
+     var_dump($_FILES["photo"]);
+
+     //check if the file is uploaded with success
+     if (isset( $_FILES["photo"]["type"]) && $_FILES["photo"]["error"]
+            ==UPLOAD_ERR_OK){  //if there is no error
+
+           $target_dir="photos/";//define folder to save the uploaded file
+           //expecting string "photos/minion.jpg"
+           $target_file=$target_dir.basename($_FILES["photos"]["name"]);
+
+           echo "<br><br>".$target_file."<br>";
+
+           //getting extension of the uploaded file
+           $file_type =pathinfo($target_file, PATHINFO_EXTENSION);
+           echo "EXTENSION:$file_type<br>";
+
+           $accepted=array("jpg","JPG", "png", "gif");
+
+           //if $file_type is not in array $accepted
+           if(!in_array($file_type,$accepted)){
+               echo "JPG only";   
+           }
+           //move the uploade file from temporary folder to project folder
+           //move uploaded_file ($_FILES["photo"]["tmp_name"]), $target_file))
+           else if(!move_uploaded_file($_FILES["photo"]["temp_name"],$target_file))
+           {
+                  echo "there was a problem uploading that photo".$_FILES["photo"]["error"];
+           }else{
+                 $query="INSERT INTO products (image, created_at)";
+                 $query="Values(?,FROM_UNIXTIME(UNIX_TIMESTAMP()))";
+
+                 $stmt=$conn->prepare($query);
+                 $stmt->bind_param('s', $target_file);
+                 if ($stmt->execute()){
+                    echo "thank you".$_POST['username'].;
+                 }   
+           }
+    }else{
+        //checking errors
+switch($_FILES["photo"]["error"]):
+    case UPLOAD_ERR_INI_SIZE:
+    $message="THE PHOTO IS LARGER";
+    break;
+ }
+
+}*/
+-->
+   
+
+
+
+ <!--
+<button type="submit" name="Previous" class="btn btn-primary" >Previous</button>
+<button type="submit" name="Next" class="btn btn-primary" >Next</button>
+ -->
 <br>
 
     <!-- Optional JavaScript; choose one of the two! -->
--- All_Pages/Controller/Catalog.inc.php
+++ All_Pages/Controller/Catalog.inc.php
@@ -1,65 +1,74 @@
 <?php
    include_once "Includes/dbconnect.inc.php";
+   
    $pagerows=3;
    if ((isset($_GET['page'])&& is_numeric($_GET['page']))){
        $current_page=htmlspecialchars($_GET['page']);
    }else{
        $current_page=1;
    }
-   $offset=($current_page=1)*$pagerows;
-   $count_query="SELECT COUNT(wine_id) as R FROM wines";
+   $offset=($current_page-1)*$pagerows;
+   
+   //Just counting wine rows
+   $count_query="SELECT COUNT(wine_name) as R FROM wines";
    $stmt=$conn->prepare($count_query);
    $stmt->execute();
    $result = $stmt->get_result()->fetch_assoc();
    
-   //var_dump($result);
-
+  
+   //displays number of rows in database for wine name
    $records=$result['R'];
-
+   //var_dump($result);
    //echo $records;
 
+   //if(7>3)
+   //{totalpages=ceil(7/3)} totalpages = 2.33 ceil rounds up to 3 total pages
+   //defualt is one page
+   
    if ($records>$pagerows){
        $total_pages=ceil($records/$pagerows);
    }else{
        $total_pages=1;
    }
 
+    /*Select a wine from wines where offset is up to 3 and page rows is 3 for page 1
+    offset is 6 on page 2 rows is 3 for page 1
+    offset is 9 but prints 1 row because its the last row*/
+
+
    $query1= "SELECT wine_name";
    $query1 .= " FROM wines";
    $query1 .= " LIMIT ?,?";
 
-   echo $query1;
+   /*Echos out all wine per row and after row limit goes to next page etc*/
+   //echo $query1;
    $stmt=$conn->prepare($query1);
    $stmt->bind_param("ii", $offset, $pagerows);
-$stmt->execute();
+    $stmt->execute();   
 
 $result=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
 if ($result){
     var_dump($result);
-
-
-foreach($result as $wines){
-    
+   
 }
-}else{
+else
+{
     echo "<p>There is no records!!! </p>";
     exit();
 }
 $stmt->close();
 if($current_page>1){
-    echo '<a href="pagination.php?page='.($current_page-1).'">Previous</a>';
+    echo '<a href="CG.php?page='.($current_page-1).'">Previous</a>';
 }
 if ($current_page<$total_pages){
-    echo'<a href="pagination.php?page'.($current_page+1).'"> Next </a>';
+    echo'<a href="CG.php?page='.($current_page+1).'"> Next </a>';
 
 }
+?>
 
 
 
-   
 
 
 
-
-   ?>
